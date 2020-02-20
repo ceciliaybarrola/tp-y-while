a1d755209bacfc0a14a1b26496c4b473b4c595c2
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 09:14:11 -0300

    Update jsIteraciones (5).js

--- jsIteraciones (5).js
+++ jsIteraciones (5).js
@@ -3,8 +3,10 @@ function mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
-
+while (sexo != "m" && sexo !="f" )
+{
+    sexo = prompt("ingrese f ó m .");
+}
 
 document.getElementById('Sexo').value=sexo;
-
 }//FIN DE LA FUNCIÓN
