33d756d3581a922cdb23e0c9d2e459bea3cabee6
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 09:12:00 -0300

    Update jsIteraciones (3).js

--- jsIteraciones (3).js
+++ jsIteraciones (3).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
 
+/*
 var clave = prompt("ingrese el número clave.");
+
+while (clave != "utn750")
+{
+    clave= prompt("ingrese el número clave.");
+    
+}
+alert("Clave correcta");
+*/
+var clave;
+ do
+ {
+    clave= prompt("ingrese la clave");
+ } while (clave != "utn750")
+
+alert("Clave correcta");
 
 
 }//FIN DE LA FUNCIÓN
