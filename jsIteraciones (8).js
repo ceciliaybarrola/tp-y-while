fa2820a287a7baca9737ee8d3194cdb64da1d2e8
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 10:00:16 -0300

    Update jsIteraciones (8).js

--- jsIteraciones (8).js
+++ jsIteraciones (8).js
@@ -4,8 +4,25 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var valor=0
 	var respuesta='si';
+	
+	do 
+	{
+	contador++;
+	valor = prompt("desea ingresar un numero?");
+	
+	if (valor >= 0)
+	{
+		positivo= positivo + parseInt(valor);
+	}
+	else
+	{
+		negativo= negativo * parseInt(valor);
+	}
+	respuesta= prompt("desea ingresar otro numero");
+	
+	}while (respuesta== "si"|| respuesta=="SI");
 
 
 document.getElementById('suma').value=positivo;
