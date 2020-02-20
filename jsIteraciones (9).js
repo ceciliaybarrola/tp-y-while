9ad4aa211d628e7e25fb3731a811bfa6ccc7275e
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 10:08:50 -0300

    Update jsIteraciones (9).js

--- jsIteraciones (9).js
+++ jsIteraciones (9).js
@@ -1,17 +1,35 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
 
-	while(respuesta!='no')
+//var contador=0;// lo saco porque no lo usamos
+var vmax;
+var vmin;
+var vnum;
+var respuesta="si"; 
+
+vnum=prompt("Ingrese un numero");
+vmin= vnum;
+vmax=vnum; // aca le asigno el valor a las variables, me falto solo eso
+respuesta=prompt("¿desea ingresar otro numero?");
+
+while (respuesta=="si"|| respuesta=="SI")
+{
+vnum=prompt("Ingrese un numero");
+
+	if (vnum > vmax)
 	{
-		
-	
+		vmax=vnum;
 	}
+	if (vnum < vmin)
+	{
+		vmin=vnum;
+	}
+respuesta=prompt("¿desea ingresar otro numero?");
+}
 
+document.getElementById("minimo").value=vmin;
+document.getElementById("maximo").value=vmax;
 
 
 
