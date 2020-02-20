36d1687da1d3c901a9318a4037c5f77be040b152
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 09:59:21 -0300

    Update jsIteraciones (6).js

--- jsIteraciones (6).js
+++ jsIteraciones (6).js
@@ -1,12 +1,34 @@
 function mostrar()
 {
 
+
 	var contador=0;
 	var acumulador=0;
 
-
+while ( contador < 5 )
+{
+	contador = contador + 1;
+	acumulador= parseInt(acumulador) + parseInt(prompt("ingrese un numero"));
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
+
+/*************************************** */
+var contador=0;
+var acumulador=0;
+var valor=0;
+var numero=5;
+
+while ( contador < 5)
+{
+	valor= prompt("ingrese un numero: ");
+	acumulador= acumulador + parseInt(valor);
+	contador++;
+}
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/numero;
+
 
 }//FIN DE LA FUNCIÓN
