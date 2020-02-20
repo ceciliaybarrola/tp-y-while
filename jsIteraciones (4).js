9bad2bdaf16c0379119ec5c22523073be81e50e1
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-20 09:13:20 -0300

    Update jsIteraciones (4).js

--- jsIteraciones (4).js
+++ jsIteraciones (4).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	
+var numero = prompt("ingrese un número entre 0 y 10.");
+
+while (numero< 0 || numero > 9)
+{
+	numero = prompt("ingrese un número entre 0 y 10.");
+}
+alert("Numero en el rango correcto");
 
 
 }//FIN DE LA FUNCIÓN
