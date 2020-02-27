8bd0a9852f97451138e7f57173caa04d054aa8f1
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-27 12:00:34 -0300

    Update jsFerreteIluminacion.js

--- jsFerreteIluminacion.js
+++ jsFerreteIluminacion.js
@@ -10,5 +10,64 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+var lampara;
+var cantidad = parseInt(document.getElementById("Cantidad").value);
+var marca= document.getElementById("Marca").value;
+var precio;
+
+
+switch (cantidad)
+{
+    case 5:
+        if (marca=="ArgentinaLuz")
+        {
+            precio=35*5- (35 *5)/100*40;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        else
+        {
+            precio=35*5- (35 *5)/100*30;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        break;
+    case 4:
+        if (marca=="ArgentinaLuz"||marca=="FelipeLamparas")
+        {
+            precio=35*4- (35 *4)/100*25;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        else
+        {
+            precio=35*4- (35 *4)/100*20;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        break;
+    case 3:
+        if (marca=="ArgentinaLuz")
+        {
+            precio=35*3- (35 *3)/100*15;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        if (marca=="FelipeLamparas")
+        {
+            precio=35*3- (35 *3)/100*10;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        else
+        {
+            precio=35*3- (35 *3)/100*5;
+            document.getElementById("precioDescuento").value=precio;
+        }
+        break;
+    case 2:
+    case 1:
+        document.getElementById("precioDescuento").value=35;
+        break;
+    case 6:
+            precio=35*6- (35 *6)/100*50;
+            document.getElementById("precioDescuento").value=precio;
+            break;
+
+}
+
 }
